c1b0ae6e300b7ca258a63b9f14fed0ae5b12f209
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2018-06-02 16:52:44 +0200

    Let the app know about convict config
    (and fix the way we start the app)

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@
 const Hapi = require('hapi');
 const h2o2 = require('h2o2');
 const routes = require('./routes');
+const config = require('./config/config.js');
 
-async function createServer (port) {
-  const server = Hapi.server({ port });
+async function createServer () {
+  const server = Hapi.server({
+    port: config.get('PORT'),
+  });
   await server.register({ plugin: h2o2 });
   server.route(routes);
   return server;
--- src/start-app.js
+++ src/start-app.js
@@ -1,16 +1,15 @@
 'use strict';
 
-const config = require('./config/config');
 const { createServer } = require('./server');
 
-const port = config.get('PORT');
-
-createServer(port)
-  .then((server) => server.start())
-  .then((server) => console.log(`Server running at: ${server.info.uri}`))
+createServer()
+  .then(async (server) => {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+  })
   .catch((e) => console.log('Failed to start server', e));
 
-module.process.on('unhandledRejection', (err) => {
+process.on('unhandledRejection', (err) => {
   console.log(err);
   process.exit(1);
 });
