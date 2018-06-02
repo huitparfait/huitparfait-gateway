164d86ab0ca000b9c5db33e0674a4ae352739001
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2018-06-02 16:24:22 +0200

    Init supertest/jest e2e testing config

--- src/server.js
+++ src/server.js
@@ -3,29 +3,12 @@
 const Hapi = require('hapi');
 const h2o2 = require('h2o2');
 const routes = require('./routes');
-const config = require('./config/config');
 
-const server = Hapi.server({
-  port: config.get('PORT'),
-});
-
-const init = async () => {
-  try {
-    await server.register({ plugin: h2o2 });
-    await server.start();
-
-    server.route(routes);
-
-    console.log(`Server running at: ${server.info.uri}`);
-  }
-  catch (e) {
-    console.log('Failed to start server', e);
-  }
+async function createServer (port) {
+  const server = Hapi.server({ port });
+  await server.register({ plugin: h2o2 });
+  server.route(routes);
+  return server;
 };
 
-process.on('unhandledRejection', (err) => {
-  console.log(err);
-  process.exit(1);
-});
-
-init();
+module.exports = { createServer };
